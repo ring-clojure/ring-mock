b57d6e59032175937e5d6677bd67d7c13857d4ec
James Reeves <jreeves@weavejester.com>
2014-12-15 23:41:44 +0000

    Add URL to project file

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject ring-mock "0.1.5"
   :description "A library for creating mock Ring request maps"
+  :url "https://github.com/ring-clojure/ring-mock"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]
