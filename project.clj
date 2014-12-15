608dc36dd578105c06babaf37888eecf8d7d1f3f
James Reeves <jreeves@weavejester.com>
2014-12-15 23:43:04 +0000

    Add Clojure 1.6.0 profile

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-codec "1.0.0"]]
   :plugins [[codox "0.6.4"]]
-  :profiles {
-    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+  :profiles
+  {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
