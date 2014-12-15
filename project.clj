03c9ac1f82595bc16c7a5c02a41c3155010ee678
James Reeves <jreeves@weavejester.com>
2014-12-15 23:54:37 +0000

    Update Codox to 0.8.10

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-codec "1.0.0"]]
-  :plugins [[codox "0.6.4"]]
+  :plugins [[codox "0.8.10"]]
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
