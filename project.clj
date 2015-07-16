c1e4a79d42ff44255c09f52894e0666bfa23e1c3
James Reeves <jreeves@weavejester.com>
2015-07-16 21:20:48 +0100

    Add Clojure 1.7 test profile

--- project.clj
+++ project.clj
@@ -7,8 +7,9 @@
                  [ring/ring-codec "1.0.0"]]
   :plugins [[codox "0.8.10"]]
   :codox {:project {:name "Ring-Mock"}}
-  :aliases {"test-all" ["with-profile" "default:+1.4:+1.5:+1.6" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.4:+1.5:+1.6:+1.7" "test"]}
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
