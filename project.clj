a0776127cb1d44f7dc6501a7c210c08e12ca1843
James Reeves <jreeves@weavejester.com>
2015-09-10 19:08:35 +0200

    Update minimum Clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -3,13 +3,11 @@
   :url "https://github.com/ring-clojure/ring-mock"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-codec "1.0.0"]]
   :plugins [[codox "0.8.13"]]
   :codox {:project {:name "Ring-Mock"}}
-  :aliases {"test-all" ["with-profile" "default:+1.4:+1.5:+1.6:+1.7" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.6:+1.7" "test"]}
   :profiles
-  {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+  {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
