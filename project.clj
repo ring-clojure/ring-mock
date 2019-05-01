4100b138af6cf96d311e771eeb37c5e8ec244d0e
James Reeves <jreeves@weavejester.com>
2019-05-01 16:17:29 +0100

    Change minimum Clojure version to 1.7

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ring-clojure/ring-mock"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.8.0"]
                  [ring/ring-codec "1.0.1"]]
   :plugins [[lein-codox "0.10.3"]]
@@ -13,8 +13,6 @@
    :source-uri  "https://github.com/ring-clojure/ring-mock/blob/{version}/{filepath}#L{line}"}
   :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9" "test"]}
   :profiles
-  {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+  {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9 {:dependencies [[org.clojure/clojure "1.9.0-RC1"]
                         [ring/ring-spec "0.0.3"]]}})
