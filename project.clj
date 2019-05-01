2b3a3914a490a0f2c42e523036b31de9ff33eb8f
James Reeves <jreeves@weavejester.com>
2019-05-01 16:18:45 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cheshire "5.8.0"]
-                 [ring/ring-codec "1.0.1"]]
+                 [cheshire "5.8.1"]
+                 [ring/ring-codec "1.1.1"]]
   :plugins [[lein-codox "0.10.3"]]
   :codox
   {:project     {:name "Ring-Mock"}
@@ -14,5 +14,5 @@
   :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9" "test"]}
   :profiles
   {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9 {:dependencies [[org.clojure/clojure "1.9.0-RC1"]
-                        [ring/ring-spec "0.0.3"]]}})
+   :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]
+                        [ring/ring-spec "0.0.4"]]}})
